f8b9f55bf1533923b963bb35dfea756e2a5a71a4
Marcel Garus <marcel.garus@gmail.com>
2019-12-13 00:21:22 +0100

    fix: wormhole receiving ack instead of waiting for actual messages

--- lib/src/wormhole.dart
+++ lib/src/wormhole.dart
@@ -19,18 +19,23 @@ class Wormhole {
         assert(appId.isNotEmpty);
 
   void _send(Map<String, dynamic> data) => _relay.sink.add(json.encode(data));
-  Future<Map<String, dynamic>> _receive() async =>
-      json.decode(await _relayReceiver.next);
+  Future<Map<String, dynamic>> _receive({String type}) async {
+    Map<String, dynamic> response;
+    do {
+      response = json.decode(await _relayReceiver.next);
+      print(response);
+    } while (type != null && response['type'] != type);
+    return response;
+  }
 
   Future<void> _initialize() async {
     _relay = IOWebSocketChannel.connect(_defaultRelayUrl);
-    _relayReceiver = StreamQueue<String>(_relay.stream);
+    _relayReceiver = StreamQueue<String>(_relay.stream.cast<String>());
 
     // TODO: choose a better side string
     _side = Random.secure().nextInt(123456789).toRadixString(16);
 
-    final welcomeMessage = await _receive();
-    assert(welcomeMessage['type'] == 'welcome');
+    final welcomeMessage = await _receive(type: 'welcome');
     assert(!(welcomeMessage['welcome'] as Map<String, dynamic>)
         .containsKey('error'));
 
@@ -43,8 +48,7 @@ class Wormhole {
 
   Future<String> _claimNameplate(String nameplate) async {
     _send({'type': 'claim', 'nameplate': nameplate});
-    final claim = await _receive();
-    assert(claim['type'] == 'claimed');
+    final claim = await _receive(type: 'claimed');
 
     final mailbox = claim['mailbox'] as String;
     assert(mailbox != null);
@@ -55,19 +59,16 @@ class Wormhole {
     await _initialize();
     _send({'type': 'allocate'});
 
-    final allocation = await _receive();
-    assert(allocation['type'] == 'allocated');
-
+    final allocation = await _receive(type: 'allocated');
     final nameplate = allocation['nameplate'] as String;
     assert(nameplate != null);
 
-    final mailbox = _claimNameplate(nameplate);
+    final mailbox = await _claimNameplate(nameplate);
     _send({'type': 'open', 'mailbox': mailbox});
 
     final key = [
       for (var i = 0; i < 3; i++) 'abc'[Random.secure().nextInt(3)],
     ].join();
-
     return '$nameplate-$key';
   }
 
@@ -75,13 +76,24 @@ class Wormhole {
     assert(code != null);
     assert(code.isNotEmpty);
     assert(code.contains('-'));
+
     final dash = code.indexOf('-');
     final nameplate = code.substring(0, dash);
     final key = code.substring(dash + 1);
+    print('Key is $key');
 
     await _initialize();
 
-    final mailbox = _claimNameplate(nameplate);
+    final mailbox = await _claimNameplate(nameplate);
     _send({'type': 'open', 'mailbox': mailbox});
+    print('Mailbox opened');
+  }
+
+  Future<void> send(String message) async {
+    _send({'type': 'add', 'phase': 'A', 'body': message});
+  }
+
+  Future<String> receive() async {
+    return (await _receive(type: 'message'))['body'];
   }
 }
