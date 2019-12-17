0940adfc88d6a047ab29e83d070f539d5ca13de7
Marcel Garus <marcel.garus@gmail.com>
2019-12-18 00:00:38 +0100

    feat: add short key generator to CodeGenerator

--- portals/lib/src/code_generators/code_generator.dart
+++ portals/lib/src/code_generators/code_generator.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:meta/meta.dart';
+
+import '../utils.dart';
 
 @immutable
 class CodePayload {
@@ -15,6 +18,15 @@ class CodePayload {
 }
 
 abstract class CodeGenerator {
+  static const _keyLength = 2;
+
+  static Uint8List generateShortKey() {
+    final random = Random.secure();
+    return [
+      for (int i = 0; i < _keyLength; i++) random.nextInt(256),
+    ].toUint8List();
+  }
+
   String payloadToCode(CodePayload payload);
   CodePayload codeToPayload(String code);
 }
