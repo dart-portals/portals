5f442b4ef22fe8352b35938d40e5fe93b08cf01b
Marcel Garus <marcel.garus@gmail.com>
2019-12-17 12:29:08 +0100

    refactor: remove PortalError, add PortalServerException and PortalCannotConnectToServerException

--- portals/lib/src/errors.dart
+++ portals/lib/src/errors.dart
@@ -1,47 +1,15 @@
-import 'dart:typed_data';
-
 import 'package:meta/meta.dart';
 
-import 'utils.dart';
-
-const _width = 60;
-
-extension _RepeatedString on String {
-  String operator *(int n) => [for (int i = 0; i < 10; i++) this].join();
-}
-
-enum Context { duringOpen }
-
-extension _ContextToString on Context {
-  String toDescriptiveString() {
-    switch (this) {
-      case Context.duringOpen:
-        return 'while opening a portal';
-      default:
-        return 'during an unknown operation';
-    }
-  }
-}
-
-class _PortalInterrupt {
-  final bool isError;
-  final Context context;
-  final String summary;
-  final String description;
-  final String suggestedFix;
-
-  _PortalInterrupt({
-    @required this.isError,
-    @required this.context,
+class PortalException implements Exception {
+  PortalException({
     @required this.summary,
     @required this.description,
     @required this.suggestedFix,
   });
 
-  String get _type => isError ? 'error' : 'exception';
-
-  String get _context =>
-      'The following $_type was thrown ${context.toDescriptiveString()}:\n';
+  final String summary;
+  final String description;
+  final String suggestedFix;
 
   String get _summary => '$summary\n\n';
 
@@ -51,7 +19,6 @@ class _PortalInterrupt {
 
   String toString() {
     return [
-      _context,
       _summary,
       _description,
       _suggestedFix,
@@ -59,81 +26,27 @@ class _PortalInterrupt {
   }
 }
 
-abstract class PortalError extends Error {
-  PortalError({
-    Context context,
-    String summary,
-    String description,
-    String suggestedFix,
-  }) : _interrupt = _PortalInterrupt(
-          isError: true,
-          context: context,
-          summary: summary,
-          description: description,
-          suggestedFix: suggestedFix,
-        );
-
-  final _PortalInterrupt _interrupt;
-
-  String toString() => _interrupt.toString();
-}
-
-class PortalException implements Exception {
-  PortalException({
-    Context context,
-    String summary,
-    String description,
-    String suggestedFix,
-  }) : _interrupt = _PortalInterrupt(
-          isError: false,
-          context: context,
-          summary: summary,
-          description: description,
-          suggestedFix: suggestedFix,
-        );
-
-  final _PortalInterrupt _interrupt;
-
-  String toString() => _interrupt.toString();
-}
-
-class NonReversibleCodeGenerator extends PortalError {
-  NonReversibleCodeGenerator({
-    @required Uint8List actualNameplate,
-    @required Uint8List actualKey,
-    @required String code,
-    @required Uint8List recreatedNameplate,
-    @required Uint8List recreatedKey,
+class PortalServerException extends PortalException {
+  PortalServerException({
+    @required String summary,
+    @required String description,
+    @required String suggestedFix,
   }) : super(
-          context: Context.duringOpen,
-          summary: 'The code generator is non-reversible.',
-          description:
-              'The code generator MyFancyCodeGenerator was asked to generate '
-              'a code for the following input:\n'
-              '  nameplate: $actualNameplate\n'
-              '  key: $actualKey\n'
-              'It responded with the code "$code".\n'
-              'However, when being asked to decode that code back into a '
-              'nameplate and key, it responded with the following:\n'
-              '  nameplate: $recreatedNameplate\n'
-              '  key: $recreatedKey\n'
-              'Note that the key differs at the byte at position TODO.',
-          suggestedFix:
-              'Make sure that when letting the code generator generate a code '
-              'for a given nameplate and key, it produces the same nameplate '
-              'and key when given the generated code.',
+          summary: summary,
+          description: description,
+          suggestedFix: suggestedFix,
         );
 }
 
-void throwSampleError() {
-  throw NonReversibleCodeGenerator(
-    actualNameplate: [21].toUint8List(),
-    actualKey: [12, 21, 32, 34, 90, 0, 21, 21, 43, 54, 11, 98, 8].toUint8List(),
-    code: 'hello-world',
-    recreatedNameplate: [21].toUint8List(),
-    recreatedKey:
-        [12, 21, 32, 39, 90, 0, 21, 21, 43, 54, 11, 98, 8].toUint8List(),
-  );
+class PortalCannotConnectToServerException extends PortalServerException {
+  PortalCannotConnectToServerException()
+      : super(
+          summary: 'The portal cannot connect to the server.',
+          description: 'Initially, the portal connects to a server. However, '
+              'we cannot connect to it.',
+          suggestedFix: 'Host your own server! And make sure you have '
+              'internet.',
+        );
 }
 
 /*
