5786ee928df11167927981d79b6e261885888e97
Marcel Garus <marcel.garus@gmail.com>
2019-12-30 19:06:47 +0100

    feature(tictactoe): add functionality to FAB

--- examples/tic_tac_toe/lib/main.dart
+++ examples/tic_tac_toe/lib/main.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 
 import 'package:portals/portals.dart';
+import 'package:portals_flutter/portals_flutter.dart';
+import 'package:version/version.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final portal = Portal();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tic Tac Toe',
+      title: 'TicTacToe',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Tic Tac Toe')),
-        floatingActionButton: FloatingActionButton.extended(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final portal = Portal(
+    appId: 'tictactoe.marcelgarus',
+    version: Version.parse('1.0.0'),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TicTacToe')),
+      floatingActionButton: Builder(
+        builder: (context) => FloatingActionButton.extended(
           icon: Icon(Icons.play_arrow),
           label: Text('Connect'),
-          onPressed: () {},
+          onPressed: () {
+            context.setupPortal(portal);
+          },
         ),
       ),
     );
