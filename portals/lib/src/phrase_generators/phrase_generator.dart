430672555ded363da0d2b5141ea8fa68255f32a6
Marcel Garus <marcel.garus@gmail.com>
2019-12-30 20:38:50 +0100

    feat: add ensureGeneratorReversible static method to PhraseGenerator

--- portals/lib/src/phrase_generators/phrase_generator.dart
+++ portals/lib/src/phrase_generators/phrase_generator.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 
 import '../utils.dart';
@@ -27,6 +28,33 @@ abstract class PhraseGenerator {
     ].toBytes();
   }
 
+  static void ensureGeneratorReversible({
+    @required PhraseGenerator generator,
+    @required PhrasePayload payload,
+    @required String generatedPhrase,
+  }) {
+    assert(generator != null);
+    assert(payload != null);
+    assert(generatedPhrase != null);
+
+    final recreatedPayload = generator.phraseToPayload(generatedPhrase);
+    final keyFits =
+        DeepCollectionEquality().equals(payload.key, recreatedPayload.key);
+    final nameplateFits = DeepCollectionEquality()
+        .equals(payload.nameplate, recreatedPayload.nameplate);
+
+    if (!keyFits || !nameplateFits) {
+      throw AssertionError('The phrase generator is non-reversible.\n'
+          'It generated the phrase $generatedPhrase for the key '
+          '${payload.key} and the nameplate ${payload.nameplate}, but when '
+          'asked to convert that phrase into the original key and nameplate, '
+          'it returned the key ${recreatedPayload.key} and the nameplate '
+          '${recreatedPayload.nameplate}.'
+          'Note that ${(!keyFits && !nameplateFits) ? 'both' : keyFits ? 'the nameplates' : 'the keys'}'
+          'differ.');
+    }
+  }
+
   String payloadToPhrase(PhrasePayload payload);
-  PhrasePayload phraseToPayload(String code);
+  PhrasePayload phraseToPayload(String phrase);
 }
