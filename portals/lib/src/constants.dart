e53093833fa546643240d5ac702bcbe65b92ef3d
Marcel Garus <marcel.garus@gmail.com>
2019-12-31 00:38:20 +0100

    feat: set WordsPhraseGenerator to be the default

--- portals/lib/src/constants.dart
+++ portals/lib/src/constants.dart
@@ -1,4 +1,4 @@
-import 'phrase_generators/hex.dart';
+import 'phrase_generators/words.dart';
 
 const defaultMailboxServerUrl = 'ws://relay.magic-wormhole.io:4000/v1';
-const defaultCodeGenerator = HexCodeGenerator();
+const defaultCodeGenerator = WordsPhraseGenerator();
