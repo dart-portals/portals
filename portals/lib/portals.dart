0f641d9ae82af196e67a8bfaae4b5c44ee64f0ed
Marcel Garus <marcel.garus@gmail.com>
2020-01-01 15:23:55 +0100

    feat: export more things like events, errors etc from the package

--- portals/lib/portals.dart
+++ portals/lib/portals.dart
@@ -1,1 +1,6 @@
 export 'src/portal.dart';
+export 'src/errors.dart';
+export 'src/events.dart';
+export 'src/phrase_generators/phrase_generator.dart';
+export 'src/phrase_generators/hex.dart';
+export 'src/phrase_generators/words.dart';
